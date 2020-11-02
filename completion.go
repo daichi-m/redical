bffaa4f3a3149cf30b3ff28713555e328c85a95a
Sohom Majumdar <Sohom.Majumdar@walmart.com>
2020-11-02 09:31:25 +0530

    Fixes around suggestions

--- completion.go
+++ completion.go
@@ -1,33 +1,44 @@
 package main
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/c-bata/go-prompt"
 )
 
+var splitter *regexp.Regexp = regexp.MustCompile("\\s+")
+
 func completer(d prompt.Document) []prompt.Suggest {
 	cmds := supported.completions
-	full := strings.TrimSpace(d.TextBeforeCursor())
-	curr := d.GetWordBeforeCursor()
-	spaced := strings.Contains(full, " ")
+	full := strings.ToUpper(d.TextBeforeCursor())
+	curr := strings.ToUpper(d.GetWordBeforeCursor())
+	parts := splitter.Split(full, -1)
+	full = strings.Join(parts, " ")
+
+	spaced := len(parts) > 1
 
 	if len(full) == 0 {
 		logger.Println("Input empty returning empty suggest")
 		return []prompt.Suggest{}
 	}
 
-	prefix := ""
-	if spaced {
-		parts := strings.Fields(full)
-		prefix = parts[0]
-		full = strings.Join(parts, " ")
-	}
 	filt := prompt.FilterHasPrefix(cmds, full, true)
 	if !spaced {
 		logger.Printf("Full Input: %s, Current Word: %s, IsSpaced: %t, Filtered Suggestions: %#v\n",
 			full, curr, spaced, LogSafeSlice(filt))
-		return filt
+		return isComplete(filt, full)
+	}
+	modFilt := filterMulti(filt, full, curr, parts)
+	return isComplete(modFilt, parts[len(parts)-1])
+}
+
+func filterMulti(filt []prompt.Suggest, full, current string, parts []string) []prompt.Suggest {
+	spaced := len(parts) > 1
+	var prefix string
+
+	if spaced {
+		prefix = parts[0]
 	}
 	modFilt := make([]prompt.Suggest, 0, len(filt))
 	for _, x := range filt {
@@ -37,8 +48,21 @@ func completer(d prompt.Document) []prompt.Suggest {
 		}
 		modFilt = append(modFilt, sugg)
 	}
-	modFilt = prompt.FilterHasPrefix(modFilt, curr, true)
+	modFilt = prompt.FilterHasPrefix(modFilt, current, true)
 	logger.Printf("Full Input: %s, Current Word: %s, Prefix: %s, IsSpaced: %t, Filtered Suggestions: %#v\n",
-		full, curr, prefix, spaced, LogSafeSlice(modFilt))
+		full, current, prefix, spaced, LogSafeSlice(modFilt))
 	return modFilt
+}
+
+func isComplete(filt []prompt.Suggest, txt string) []prompt.Suggest {
+	if len(filt) != 1 {
+		return filt
+	}
+
+	filt1 := filt[0]
+	logger.Printf("Suggestions: %#v, Full Text: %s\n", filt, txt)
+	if filt1.Text == txt {
+		return []prompt.Suggest{}
+	}
+	return filt
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,8 @@ module github.com/daichi-m/redical
 go 1.14
 
 require (
+	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/c-bata/go-prompt v0.2.5
-	github.com/gomodule/redigo v1.8.2
-	github.com/jroimartin/gocui v0.4.0
-	github.com/nsf/termbox-go v0.0.0-20200418040025-38ba6e5628f1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
